609997f2d9423641b9dc78e768cab95c265ba10e
Tim Morgan <tim@timmorgan.org>
2019-08-30 15:36:33 -0500

    2018 day 5

---
+++ 2018/day05.rb
@@ -0,0 +1,10 @@
+input=ARGF.read.strip
+i=input.dup
+while i.gsub!(/aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ|Aa|Bb|Cc|Dd|Ee|Ff|Gg|Hh|Ii|Jj|Kk|Ll|Mm|Nn|Oo|Pp|Qq|Rr|Ss|Tt|Uu|Vv|Ww|Xx|Yy|Zz/, ''); end
+p i.size
+all=('a'..'z').map do |l|
+  i=input.gsub(/#{l}/i, '')
+  while i.gsub!(/aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ|Aa|Bb|Cc|Dd|Ee|Ff|Gg|Hh|Ii|Jj|Kk|Ll|Mm|Nn|Oo|Pp|Qq|Rr|Ss|Tt|Uu|Vv|Ww|Xx|Yy|Zz/, ''); end
+  i.size
+end
+p all.sort.first
